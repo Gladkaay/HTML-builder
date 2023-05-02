cc3b6bb0b941b7a028610d589797460388ada032
Nastya <nastya04052001@gmail.com>
2023-05-02 15:54:36 +0300

    feat: add task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const fs = require('fs');
+
+fs.writeFile(path.join(__dirname, 'hello.txt'), "", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log("Вау, все комплименты мира для тебя! Напиши что-нибудь:")
+});
+
+const { stdin, stdout } = process;
+stdin.on('data', data => {
+  if (data.toString().trim() == 'exit') {
+    stdout.write('Удачи!'); 
+    process.exit();
+  }
+  fs.appendFile(
+    path.join(__dirname, 'hello.txt'),
+    data,
+    err => {
+      if (err) throw err;
+    }
+  );
+});
+process.on('SIGINT', () => {
+  stdout.write('Пока пока!'); 
+  process.exit();
+});
