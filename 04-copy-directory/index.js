f68b0937f729b856a735bafaf9dab9c3ed4b5fdc
Nastya <nastya04052001@gmail.com>
2023-05-03 11:59:12 +0300

    feat: add task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const path = require('path');
+const fs = require('fs');
 
+fs.promises.mkdir(path.join(__dirname, 'files-copy'), {recursive: true});
+fs.promises.readdir(path.join(__dirname, 'files-copy'))
+  .then(filesCopy => {
+      for (let fileCopy of filesCopy) {
+        fs.unlink(path.join(__dirname, 'files-copy', fileCopy), (err) => {
+          if (err) throw err;
+        });
+      }
+  })
+fs.promises.readdir(path.join(__dirname, 'files'))
+  .then(files => {
+      for (let file of files) {
+        fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+      }
+  })
